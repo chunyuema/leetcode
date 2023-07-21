79319964f6ec4efd11c4d9a9eb7500b03b39e13e
Chunyue Ma <cma01@wesleyan.edu>
2023-07-20 20:51:43 -0700

    Bottom up approach for q45

---
+++ q0045/q0045.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int jump(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (j + nums[j] >= i) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp[nums.length - 1];
+    }
+}
