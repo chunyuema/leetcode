01f6ceaa1923e1e80b7c9c02278b51b0e619256a
Chunyue Ma <cma01@wesleyan.edu>
2023-07-24 20:51:06 -0700

    Java solution for 46

---
+++ q0046/q0046.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Set<Integer> used = new HashSet<>();
+        backTrackHelper(nums, res, new ArrayList<>(), used);
+        return res;
+    }
+
+    public void backTrackHelper(int[] nums, List<List<Integer>> res, List<Integer> temp, Set<Integer> used) {
+        if (temp.size() == nums.length) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!used.contains(i)) {
+                temp.add(nums[i]);
+                used.add(i);
+                backTrackHelper(nums, res, temp, used);
+                temp.remove(temp.size() - 1);
+                used.remove(i);
+            }
+        }
+    }
+}
