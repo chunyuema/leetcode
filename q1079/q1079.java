cc7b8517ba611188275af9d0347216bb45029577
Chunyue Ma <cma01@wesleyan.edu>
2023-07-29 21:31:10 -0700

    Remove the unused idx

--- q1079/q1079.java
+++ q1079/q1079.java
@@ -2,20 +2,18 @@ class Solution {
     public int numTilePossibilities(String tiles) {
         Set<String> seqSet = new HashSet<>();
         int[] used = new int[tiles.length()];
-        backtrackHelper(tiles, seqSet, 0, new StringBuilder(), used);
-        return seqSet.size();
+        backtrackHelper(tiles, seqSet, new StringBuilder(), used);
+        return seqSet.size() - 1;
     }
 
-    private void backtrackHelper(String tiles, Set<String> seqSet, int idx, StringBuilder temp, int[] used) {
-        if (temp.length() > 0) {
-            seqSet.add(temp.toString());
-        }
+    private void backtrackHelper(String tiles, Set<String> seqSet, StringBuilder temp, int[] used) {
+        seqSet.add(temp.toString());
         for (int i = 0; i < tiles.length(); i++) {
             if (used[i] != 0)
                 continue;
             temp.append(tiles.charAt(i));
             used[i] = 1;
-            backtrackHelper(tiles, seqSet, idx + 1, temp, used);
+            backtrackHelper(tiles, seqSet, temp, used);
             temp.deleteCharAt(temp.length() - 1);
             used[i] = 0;
         }
