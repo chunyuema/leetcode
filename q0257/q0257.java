78ebf2f652b88747b29f1d0872822ceee4963be6
Chunyue Ma <cma01@wesleyan.edu>
2023-08-02 20:02:06 -0700

    Update code

--- q0257/q0257.java
+++ q0257/q0257.java
@@ -1,41 +1,18 @@
-package q0257;
-
-import java.util.*;
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         ArrayList<String> res = new ArrayList<>();
-        getPaths(root, res, "");
+        dfsHelper(root, res, "");
         return res;
     }
 
-    private void getPaths(TreeNode node, ArrayList<String> res, String currPath) {
+    private void dfsHelper(TreeNode node, ArrayList<String> res, String currPath) {
         if (node == null)
             return;
         if (node.left == null && node.right == null) {
             res.add(currPath + String.valueOf(node.val));
             return;
         }
-        getPaths(node.left, res, currPath + String.valueOf(node.val) + "->");
-        getPaths(node.right, res, currPath + String.valueOf(node.val) + "->");
+        dfsHelper(node.left, res, currPath + String.valueOf(node.val) + "->");
+        dfsHelper(node.right, res, currPath + String.valueOf(node.val) + "->");
     }
 }
