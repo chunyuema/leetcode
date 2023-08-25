f0ff51d97f321bc1639cd320123aa8358e437b20
Chunyue Ma <cma01@wesleyan.edu>
2023-08-25 16:45:52 -0700

    Java 97

---
+++ q0097/q0097-bottom-up.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) return false;
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+        // base case: the last cell in the DP array is initialized as 1, this is because if the pointer on s1 and s2 are already at s1.length and s2.length(), then s3 has been successfully constructed with substrings from s1 and s2
+        dp[s1.length()][s2.length()] = 1;
+
+        for (int i = s1.length(); i >= 0; i--) {
+            for (int j = s2.length(); j >= 0; j--) {
+                // case 1: If current character from s1 matches that of s3, then take the solution from one cell down 
+                if (i < s1.length() && s1.charAt(i) == s3.charAt(i+j)) dp[i][j] |= dp[i+1][j];
+                // case 2: If current character from s2 matches that of s3, then take the solution from one cell left
+                if (j < s2.length() && s2.charAt(j) == s3.charAt(i+j)) dp[i][j] |= dp[i][j+1];
+            }
+        }
+
+        return dp[0][0] == 1;
+    }
+}
