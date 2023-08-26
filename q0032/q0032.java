b1269321087bf7ca11f250c07ac211994466f748
Chunyue Ma <cma01@wesleyan.edu>
2023-08-25 17:10:43 -0700

    Add comments

--- q0032/q0032.java
+++ q0032/q0032.java
@@ -8,11 +8,16 @@ class Solution {
             if (s.charAt(i) == ')') {
                 if (s.charAt(i - 1) == '(') {
                     dp[i] = (i - 2 > 0 ? dp[i - 2] : 0) + 2;
-                } else // s.charAr(i-1) == ')'
+                } 
+                else // s.charAr(i-1) == ')'
                 {
+                    // find the possible '(' that matches with the current ')'
                     int matchingOpen = i - dp[i - 1] - 1;
                     if (matchingOpen >= 0 && s.charAt(matchingOpen) == '(') {
+                        // part 1: if matching '(' can be found then add 2 to its value
                         dp[i] = dp[i - 1] + 2;
+
+                        // part 2: add the longest valid string prior to the position of the matching valid '('
                         dp[i] += ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0);
                     }
 
