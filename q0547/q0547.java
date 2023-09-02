e075552214d40172063e83e9378a974993e5d69c
Chunyue Ma <cma01@wesleyan.edu>
2023-09-02 15:34:08 -0700

    Java 547

---
+++ q0547/q0547.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int[] isVisited = new int[isConnected.length];
+        int numOfProvince = 0;
+        for (int i = 0; i < isConnected.length; i++) {
+            // if the current city has not been explore, explore everything that forms an
+            // province with it
+            if (isVisited[i] != 1) {
+                numOfProvince++;
+                dfsHelper(isConnected, isVisited, i);
+            }
+        }
+        return numOfProvince;
+    }
+
+    private void dfsHelper(int[][] isConnected, int[] isVisited, int i) {
+        // mark the current city as visited
+        isVisited[i] = 1;
+
+        // loop through other cities to look for the ones that is connected to the
+        // current city
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[i][j] == 1 && isVisited[j] != 1) {
+                dfsHelper(isConnected, isVisited, j);
+            }
+        }
+
+        return;
+    }
+}
