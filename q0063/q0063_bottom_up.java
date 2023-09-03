8820552b96c871931b5185dd7a30594030d53e97
Chunyue Ma <cma01@wesleyan.edu>
2023-09-03 08:42:18 -0700

    Java 63

---
+++ q0063/q0063_bottom_up.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // If you are on the first row / first column: there is only 1 way to reach the cell
+                if (i == 0 || j == 0) dp[i][j] = 1;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+}
