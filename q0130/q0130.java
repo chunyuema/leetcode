8877916c1d0441703360c42f850a53ce62e71a71
Chunyue Ma <cma01@wesleyan.edu>
2023-09-03 16:28:32 -0700

    Java 130

---
+++ q0130/q0130.java
@@ -0,0 +1,41 @@
+class Solution {
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (r == 0 || c == 0 || r == m - 1 || c == n - 1) {
+                    if (board[r][c] == 'O') {
+                        captureNonSurroundedRegions(board, r, c, m, n);
+                    }
+                }
+            }
+        }
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (board[r][c] == 'O') {
+                    board[r][c] = 'X';
+                }
+            }
+        }
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (board[r][c] == 'T') {
+                    board[r][c] = 'O';
+                }
+            }
+        }
+    }
+
+    private void captureNonSurroundedRegions(char[][] board, int r, int c, int m, int n) {
+        if (r < 0 || c < 0 || r == m || c == n || board[r][c] != 'O')
+            return;
+        board[r][c] = 'T';
+        captureNonSurroundedRegions(board, r + 1, c, m, n);
+        captureNonSurroundedRegions(board, r - 1, c, m, n);
+        captureNonSurroundedRegions(board, r, c + 1, m, n);
+        captureNonSurroundedRegions(board, r, c - 1, m, n);
+    }
+}
