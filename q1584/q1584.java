f19b013960a0239c56720c9846d8befec0c7160c
Chunyue Ma <cma01@wesleyan.edu>
2023-09-03 20:44:04 -0700

    Add comments

--- q1584/q1584.java
+++ q1584/q1584.java
@@ -1,7 +1,8 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int n = points.length;
 
+        // Construct the graph (bidirectional in this case)
+        int n = points.length;
         Map<Integer, List<int[]>> graph = new HashMap<>();
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -13,12 +14,18 @@ class Solution {
 
         int minCost = 0;
         Set<Integer> visited = new HashSet<>();
+
+        // Use a min heap to track which one of the node should be explored the next
         PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
         minHeap.offer(new int[] { 0, 0 });
         while (visited.size() < n) {
+
+            // Get the information of the next node to be exxplore: the one with the minimum cost to extend to
             int nextNode = (minHeap.peek())[1];
             int costToNextNode = (minHeap.peek())[0];
             minHeap.poll();
+
+            // Only explore a node if it has not been previously explored
             if (!visited.contains(nextNode)) {
                 minCost += costToNextNode;
                 visited.add(nextNode);
