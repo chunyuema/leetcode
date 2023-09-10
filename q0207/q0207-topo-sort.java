bee8a7564a28547d229212c678f7c45264d27462
Chunyue Ma <cma01@wesleyan.edu>
2023-09-10 14:02:16 -0700

    Add comments for topological sort

--- q0207/q0207-topo-sort.java
+++ q0207/q0207-topo-sort.java
@@ -5,13 +5,16 @@ class Solution {
         for (int[] pair : prerequisites) {
             int course = pair[0];
             int prereqCourse = pair[1];
-            adjList.computeIfAbsent(prereqCourse, k->new ArrayList<>()).add(course);
+            adjList.computeIfAbsent(prereqCourse, k -> new ArrayList<>()).add(course);
             indegree[course]++;
         }
 
+        // Start with all the nodes with in degree = 0 (These courses have no
+        // prerequiste)
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) queue.offer(i);
+            if (indegree[i] == 0)
+                queue.offer(i);
         }
 
         int numTaken = 0;
@@ -19,7 +22,12 @@ class Solution {
             int currCourse = queue.poll();
             numTaken++;
             for (int n : adjList.getOrDefault(currCourse, new ArrayList<>())) {
+                // After taking this currCourse, all the courses which have the current course
+                // as a prerequisite can have their in degree decreased by 1.
                 indegree[n]--;
+
+                // Add the course to the queue if the indegree becomes 0 (No more pre-requisite,
+                // hence the course is ready to be taken)
                 if (indegree[n] == 0) {
                     queue.offer(n);
                 }
--- q0210/q0210-topo-sort.java
+++ q0210/q0210-topo-sort.java
@@ -1,3 +1,6 @@
+/*
+ * Please see 207 for comments explaining the topological sort
+ */
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] indegree = new int[numCourses];
@@ -5,19 +8,22 @@ class Solution {
         for (int[] pair : prerequisites) {
             int course = pair[0];
             int prereqCourse = pair[1];
-            adjList.computeIfAbsent(prereqCourse, k->new ArrayList<>()).add(course);
+            adjList.computeIfAbsent(prereqCourse, k -> new ArrayList<>()).add(course);
             indegree[course]++;
         }
 
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) queue.offer(i);
+            if (indegree[i] == 0)
+                queue.offer(i);
         }
 
         int numTaken = 0;
         int[] courseSeq = new int[numCourses];
         while (!queue.isEmpty()) {
             int currCourse = queue.poll();
+            // Record the current course to be taken as we are tracking a sequence of
+            // courses
             courseSeq[numTaken] = currCourse;
             numTaken++;
             for (int n : adjList.getOrDefault(currCourse, new ArrayList<>())) {
@@ -27,7 +33,10 @@ class Solution {
                 }
             }
         }
-        if (numTaken == numCourses) return courseSeq;
+
+        // Only return the courseSeq if we can successfully finish all the courses
+        if (numTaken == numCourses)
+            return courseSeq;
         return new int[0];
     }
 }
