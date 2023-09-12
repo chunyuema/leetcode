9aac3d2a8e10c464b5460632e87a14f6c4021cbe
Chunyue Ma <cma01@wesleyan.edu>
2023-09-11 22:14:45 -0700

    Rename variable and add comments

--- q1514/q1514.java
+++ q1514/q1514.java
@@ -1,14 +1,7 @@
-package q1514;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+
+        // Build the graph based on edges given
         Map<Integer, List<Pair<Integer, Double>>> graph = new HashMap<>();
         for (int i = 0; i < edges.length; i++) {
             int n1 = edges[i][0];
@@ -17,8 +10,9 @@ class Solution {
             graph.computeIfAbsent(n2, k -> new ArrayList<>()).add(new Pair<>(n1, succProb[i]));
         }
 
-        double[] maxProdToNode = new double[n];
-        maxProdToNode[start] = 1;
+        // maxProdToNode tracks the maximum probability each node i in the graph
+        double[] maxProbToNode = new double[n];
+        maxProbToNode[start] = 1;
 
         Queue<Integer> queue = new LinkedList<>();
         queue.add(start);
@@ -28,13 +22,15 @@ class Solution {
             for (Pair<Integer, Double> neighbor : graph.getOrDefault(currNode, new ArrayList<>())) {
                 int nextNode = neighbor.getKey();
                 double probToNextNode = neighbor.getValue();
-                double newProbToNextNode = maxProdToNode[currNode] * probToNextNode;
-                if (newProbToNextNode > maxProdToNode[nextNode]) {
-                    maxProdToNode[nextNode] = newProbToNextNode;
+                // Get the new probability to the next node, and update the prob to the next
+                // node if it is smaller => Need to add the node back to the queue as well
+                double newProbToNextNode = maxProbToNode[currNode] * probToNextNode;
+                if (newProbToNextNode > maxProbToNode[nextNode]) {
+                    maxProbToNode[nextNode] = newProbToNextNode;
                     queue.add(nextNode);
                 }
             }
         }
-        return maxProdToNode[end];
+        return maxProbToNode[end];
     }
 }
