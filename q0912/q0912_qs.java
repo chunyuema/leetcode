b6c11a94bf5d9e2408eaf31926f38d72bc452778
Chunyue Ma <cma01@wesleyan.edu>
2023-09-13 21:58:49 -0700

    Update file and add comments

--- q0912/q0912_qs.java
+++ q0912/q0912_qs.java
@@ -15,14 +15,22 @@ class Solution {
     private int partition(int[] nums, int l, int r) {
         int m = l + (r - l) / 2;
         int pivot = nums[m];
+
+        // move the pivot number temporarily to the right most position
         swap(nums, m, r);
+
+        // start from the left most position
         int p = l;
         for (int i = l; i < r; i++) {
+            // if we find any number that is smaller than pivot number, move it to the left
+            // most possible, increment the pivot position
             if (nums[i] <= pivot) {
                 swap(nums, p, i);
                 p++;
             }
         }
+
+        // move the pivot back to position p, and return the pivot position
         swap(nums, p, r);
         return p;
     }
