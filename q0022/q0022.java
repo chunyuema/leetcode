695e7a3c7713dcf1110cece82f2db7c039da826e
Chunyue Ma <cma01@wesleyan.edu>
2023-09-14 21:05:28 -0700

    Refactor and add comments

--- q0022/q0022.java
+++ q0022/q0022.java
@@ -1,28 +1,31 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList();
-        backtrack(res, new StringBuilder(), n, n);
+        backtrackHelper(res, new StringBuilder(), n, n);
         return res;
     }
 
-    public void backtrack(List<String> res,
-            StringBuilder temp,
-            int openP, int closeP) {
-        if (openP == 0 && closeP == 0) {
+    public void backtrackHelper(List<String> res, StringBuilder temp, int open, int close) {
+
+        // base case: if open > close, or either one is less than zero, we violated the
+        // rule, return immediately without further investigation
+        if (open < 0 || close < 0 || open > close)
+            return;
+
+        // base case: if both are zero, we have constructed a valid parentheses string
+        if (open == 0 && close == 0) {
             res.add(temp.toString());
             return;
         }
 
-        if (openP > 0) {
-            temp.append("(");
-            backtrack(res, temp, openP - 1, closeP);
-            temp.deleteCharAt(temp.length() - 1);
-        }
+        // backtracking steps to add a ( to the current string
+        temp.append("(");
+        backtrackHelper(res, temp, open - 1, close);
+        temp.deleteCharAt(temp.length() - 1);
 
-        if (closeP > 0 && closeP > openP) {
-            temp.append(")");
-            backtrack(res, temp, openP, closeP - 1);
-            temp.deleteCharAt(temp.length() - 1);
-        }
+        // backtracking steps to add a ) to the current string
+        temp.append(")");
+        backtrackHelper(res, temp, open, close - 1);
+        temp.deleteCharAt(temp.length() - 1);
     }
 }
