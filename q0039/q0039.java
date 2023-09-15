23b6e1cf543ef1b7c1a7f407adc7fb52e87f3e30
Chunyue Ma <cma01@wesleyan.edu>
2023-09-14 20:33:57 -0700

    Refactor and add comments

--- q0039/q0039.java
+++ q0039/q0039.java
@@ -6,22 +6,29 @@ class Solution {
         return res;
     }
 
-    private void backtrackHelper(int[] candidates,
-            List<List<Integer>> res,
-            List<Integer> temp,
-            int currDiff,
-            int i) {
+    private void backtrackHelper(int[] candidates, List<List<Integer>> res, List<Integer> temp, int currDiff, int i) {
+        // base case: invalid
+        if (currDiff < 0)
+            return;
+
+        // base case: if we get the target
         if (currDiff == 0) {
             res.add(new ArrayList(temp));
             return;
         }
+
+        // start looping from index i since the ordering in combination does not matter
         for (int j = i; j < candidates.length; j++) {
+            // move ahead: take the candidate, and add it to the temp
             int newDiff = currDiff - candidates[j];
-            if (newDiff >= 0) {
-                temp.add(candidates[j]);
-                backtrackHelper(candidates, res, temp, newDiff, j);
-                temp.remove(temp.size() - 1);
-            }
+            temp.add(candidates[j]);
+
+            // recursively call from the same i because one number can be used unlimited
+            // amount of times
+            backtrackHelper(candidates, res, temp, newDiff, j);
+
+            // backtrack: remove the number from the temp
+            temp.remove(temp.size() - 1);
         }
     }
 }
