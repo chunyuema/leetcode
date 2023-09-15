bbe99984092b045cd3d08eab4d3d43f3874b09e8
Chunyue Ma <cma01@wesleyan.edu>
2023-09-14 20:47:39 -0700

    Add comments

--- q0077/q0077.java
+++ q0077/q0077.java
@@ -6,14 +6,22 @@ class Solution {
     }
 
     private void backtrackHelper(int n, int k, int idx, List<List<Integer>> res, List<Integer> temp) {
+        // check for the base case: if the length satisfy the condition, one valid
+        // combination has been created => add to solution
         if (temp.size() == k) {
             res.add(new ArrayList<>(temp));
             return;
         }
 
+        // start looping from the current position:
         for (int i = idx; i <= n; i++) {
+            // move ahead: add the num to temp
             temp.add(i);
+
+            // recurse on the backtrack function
             backtrackHelper(n, k, i + 1, res, temp);
+
+            // backtrack: remove the num from temp
             temp.remove(temp.size() - 1);
         }
     }
--- q0078/q0078.java
+++ q0078/q0078.java
@@ -6,10 +6,21 @@ class Solution {
     }
 
     public void backtrackHelper(List<List<Integer>> res, int[] nums, int start, List<Integer> currTmp) {
+        // since we are generating subsets only, there is not need to check for any
+        // condition => simply add it to the final solution
         res.add(new ArrayList<>(currTmp));
+
+        // loop from the start (ordering does not matter, so do not need to repeat
+        // previous ones before index start)
         for (int i = start; i < nums.length; i++) {
+
+            // move ahead
             currTmp.add(nums[i]);
+
+            // recursive call
             backtrackHelper(res, nums, i + 1, currTmp);
+
+            // backtrack
             currTmp.remove(currTmp.size() - 1);
         }
     }
