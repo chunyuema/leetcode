f559d1d4ed4942710c3b6ab6c712df351900c13e
Chunyue Ma <cma01@wesleyan.edu>
2023-09-15 17:31:04 -0700

    Java 872

---
+++ q0872/q0872.java
@@ -0,0 +1,21 @@
+class Solution {
+    private List<Integer> tree1Leaves = new ArrayList<>();
+    private List<Integer> tree2Leaves = new ArrayList<>();
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        dfsHelper(root1, tree1Leaves);
+        dfsHelper(root2, tree2Leaves);
+        return tree1Leaves.equals(tree2Leaves);
+    }
+
+    private void dfsHelper(TreeNode node, List<Integer> leaves) {
+        if (node == null)
+            return;
+        if (node.left == null && node.right == null) {
+            leaves.add(node.val);
+            return;
+        }
+        dfsHelper(node.left, leaves);
+        dfsHelper(node.right, leaves);
+    }
+}
