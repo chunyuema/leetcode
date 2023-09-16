57eb87df26a29802b7efd4b665ab6227045ee889
Chunyue Ma <cma01@wesleyan.edu>
2023-09-15 20:50:28 -0700

    Rename symbol and add comments

--- q1161/q1161.java
+++ q1161/q1161.java
@@ -1,15 +1,17 @@
 class Solution {
     public int maxLevelSum(TreeNode root) {
         int currMaxSum = root.val;
-        int maxSumLevel = 1;
+        int maxSum = 1;
         int currLevel = 1;
 
         Deque<TreeNode> q = new ArrayDeque<TreeNode>();
         q.offer(root);
 
         while (!q.isEmpty()) {
+            // important to get n as this is the number of nodes on the current level
             int n = q.size();
             int levelSum = 0;
+            // use n as a loop guard to terminate the loop
             for (int i = 0; i < n; i++) {
                 TreeNode node = q.poll();
                 levelSum += node.val;
@@ -21,11 +23,11 @@ class Solution {
 
             if (levelSum > currMaxSum) {
                 currMaxSum = levelSum;
-                maxSumLevel = currLevel;
+                maxSum = currLevel;
             }
             currLevel++;
         }
 
-        return maxSumLevel;
+        return maxSum;
     }
 }
