efaa3f6468175de3bc40421043d4c2d3113634e3
Chunyue Ma <cma01@wesleyan.edu>
2023-09-17 17:15:30 -0700

    Add comments

--- q0064/q0064_bottom_up.java
+++ q0064/q0064_bottom_up.java
@@ -4,17 +4,21 @@ class Solution {
         int n = grid[0].length;
         int[][] dp = new int[m][n];
 
-        // Preprocessing the dp array
+        // preprocessing the dp array since there are no other options on the first
+        // column and first row, go straight down / go straight right
         dp[0][0] = grid[0][0];
-        for (int i = 1; i < m; i++) dp[i][0] = dp[i-1][0] + grid[i][0];
-        for (int j = 1; j < n; j++) dp[0][j] = dp[0][j-1] + grid[0][j];
-        
+        for (int i = 1; i < m; i++)
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        for (int j = 1; j < n; j++)
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                dp[i][j] = Math.min(dp[i][j-1], dp[i-1][j]) + grid[i][j];
+                dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
             }
         }
 
-        return dp[m-1][n-1];
+        // dp[m-1][n-1] represents the min path sum to reach the right bottom corner
+        return dp[m - 1][n - 1];
     }
 }
--- q0064/q0064_top_down.java
+++ q0064/q0064_top_down.java
@@ -2,23 +2,31 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] memo = new int[m][n];
-        for (int[] row : memo) Arrays.fill(row, -1);
 
-        return dpHelper(m-1, n-1, grid, memo);
+        // prepare the memo for the dpHelper
+        int[][] memo = new int[m][n];
+        for (int[] row : memo)
+            Arrays.fill(row, -1);
+
+        return dpHelper(m - 1, n - 1, grid, memo);
     }
 
-
     private int dpHelper(int i, int j, int[][] grid, int[][] memo) {
-        // Base cases: 
-        if (i == 0 && j == 0) return grid[i][j];
-        // This case catches the function call that has invalid i / j
-        if (i < 0 || j < 0) return Integer.MAX_VALUE;
+        // Base cases:
+        if (i == 0 && j == 0)
+            return grid[i][j];
 
-        if (memo[i][j] != -1) return memo[i][j];
+        // catches the function call that has invalid i / j
+        // this happens when calling dpHelper from the first column / row
+        if (i < 0 || j < 0)
+            return Integer.MAX_VALUE;
 
-        // Two recursive cases, one from going to right, and one from going to left. 
-        memo[i][j] = grid[i][j] + Math.min(dpHelper(i-1, j, grid, memo), dpHelper(i, j-1, grid, memo));
+        // use memoized value to prevent time complexity
+        if (memo[i][j] != -1)
+            return memo[i][j];
+
+        // Two recursive cases, one from going to right, and one from going to left.
+        memo[i][j] = grid[i][j] + Math.min(dpHelper(i - 1, j, grid, memo), dpHelper(i, j - 1, grid, memo));
         return memo[i][j];
     }
 }
