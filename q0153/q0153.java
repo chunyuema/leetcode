5fa16bf92fc6c7f2c0e9d93b6de21516c6e6f263
Chunyue Ma <cma01@wesleyan.edu>
2023-09-19 21:25:02 -0700

    Update solution for 153

--- q0153/q0153.java
+++ q0153/q0153.java
@@ -2,21 +2,13 @@ class Solution {
     public int findMin(int[] nums) {
         int l = 0;
         int r = nums.length - 1;
-        if (r == 0 || nums[r] >= nums[l]) {
-            return nums[0];
-        }
-
         while (l < r) {
             int m = l + (r - l) / 2;
-            if (nums[m] > nums[m + 1])
-                return nums[m + 1];
-            if (nums[m] < nums[m - 1])
-                return nums[m];
-            if (nums[m] > nums[0])
+            if (nums[m] > nums[r])
                 l = m + 1;
-            if (nums[m] < nums[0])
-                r = m - 1;
+            if (nums[m] < nums[r])
+                r = m;
         }
-        return -1;
+        return nums[l];
     }
 }
