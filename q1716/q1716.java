de563b63a151274bb802283c68c6069cd8dc4c9f
Chunyue Ma <cma01@wesleyan.edu>
2023-12-05 20:33:47 -0800

    Code 1716

---
+++ q1716/q1716.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int totalMoney(int n) {
+        // compute the total money for full weeks
+        int fullWeekNum = n / 7;
+        int first = 28;
+        int last = 28 + 7 * (fullWeekNum - 1);
+        int fullWeekTotal = (first + last) * fullWeekNum / 2;
+
+        // compute the money for the left over days
+        int lastWeekTotal = 0;
+        for (int i = 1 + fullWeekNum; i < 1 + fullWeekNum + n % 7; i++) {
+            lastWeekTotal += i;
+        }
+        return fullWeekTotal + lastWeekTotal;
+    }
+}
