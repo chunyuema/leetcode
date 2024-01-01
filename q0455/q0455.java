9ae0d237c1640c39b3a3e4048e115b78e62760de
Chunyue Ma <cma01@wesleyan.edu>
2023-12-31 16:38:38 -0800

    Update comments and refactor

--- q0455/q0455.java
+++ q0455/q0455.java
@@ -5,9 +5,13 @@ class Solution {
         int cookieIndex = 0;
         int childIndex = 0;
         while (cookieIndex < s.length && childIndex < g.length) {
+            // try to use the smallest cookie to match the current kid with smallest greed
             if (s[cookieIndex] >= g[childIndex]) {
                 childIndex++;
             }
+            // update the cookieIndex
+            // case 1: satisfied the kid and need a new cookie for the next kid
+            // case 2: kid's greed factor larger than current cookie, need a bigger cookie
             cookieIndex++;
         }
         return childIndex;
--- q0455/q0455.py
+++ q0455/q0455.py
@@ -1,9 +1,12 @@
 def findContentChildren(g: List[int], s: List[int]) -> int:
     s.sort()
     g.sort()
-    child = cookie = 0
-    while child < len(g) and cookie < len(s):
-        if s[cookie] >= g[child]:
-            child += 1
-        cookie += 1
-    return child
+    childIndex = cookieIndex = 0
+    while childIndex < len(g) and cookieIndex < len(s):
+        if s[cookieIndex] >= g[childIndex]:
+            childIndex += 1
+        # update the cookieIndex 
+        # case 1: satisfied the kid and need a new cookie for the next kid
+        # case 2: kid's greed factor larger than current cookie, need a bigger cookie
+        cookieIndex += 1
+    return childIndex
