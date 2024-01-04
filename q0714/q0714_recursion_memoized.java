5dd053ebf4c6a42804999a61e3e0f7f2163774db
Chunyue Ma <cma01@wesleyan.edu>
2024-01-04 08:40:12 -0800

    Add rescursive approach with memoization for 714

---
+++ q0714/q0714_recursion_memoized.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int[][] memo = new int[prices.length][2];
+        for (int[] row : memo)
+            Arrays.fill(row, -1);
+        return dpHelper(prices, fee, 0, 0, memo);
+    }
+
+    // Given a current stock index i, and the status of whether a user has stock
+    // Recursively find out the best profit that is returned if user starts trading
+    private int dpHelper(int[] prices, int fee, int i, int hasStock, int[][] memo) {
+
+        // Base Case: If there is no more stock, the max profit is 0
+        if (i == prices.length)
+            return 0;
+
+        // Memoized step, if dp(i, hasStock) has been computed, return result
+        if (memo[i][hasStock] != -1)
+            return memo[i][hasStock];
+
+        // Recursive Step:
+        // If customer does not have a stock, the only way to change is to buy a stock
+        // If customer cannot buy, set this result to 0 to ensure that it is ignored
+        int buy = hasStock == 1 ? 0 : dpHelper(prices, fee, i + 1, 1, memo) - prices[i];
+
+        // If customer does have a stock, the only way to change is to sell the stock
+        // If customer cannot sell, set this result to 0 to ensure that it is ignored
+        int sell = hasStock == 1 ? dpHelper(prices, fee, i + 1, 0, memo) + prices[i] - fee : 0;
+
+        // Otherwise, the customer could choose to ignore today and maintain status quo
+        int hold = dpHelper(prices, fee, i + 1, hasStock, memo);
+
+        // Update the memo and return the answer
+        memo[i][hasStock] = Math.max(Math.max(buy, sell), hold);
+        return memo[i][hasStock];
+    }
+}
