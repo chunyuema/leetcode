b80fbaff52489241dbe18e6cb74063d4bd05f522
Chunyue Ma <cma01@wesleyan.edu>
2024-01-04 22:34:47 -0800

    Add comments and rename file

--- q0300/q0300_iterative_tabulation.java
+++ q0300/q0300_iterative_tabulation.java
@@ -1,15 +1,23 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
+
+        // Let dp[i] represent the max len of LIS ending at index i
         int[] dp = new int[nums.length];
         int maxLen = 1;
         Arrays.fill(dp, 1);
+
+        // To compute the len of LIS ending at i
         for (int i = 0; i < nums.length; i++) {
+
+            // Recurrence relation: dp[i] = max(dp[j] + 1) for all j (nums[i] > nums[j])
+            // Loop through all possible j (j < i), and update the dp[i] accordingly
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
 
+            // Update the max len to return it at the end of the loop
             maxLen = Math.max(dp[i], maxLen);
         }
         return maxLen;
