e451bf1a1456522c0621b6d45fe65bdc93bb1276
Chunyue Ma <cma01@wesleyan.edu>
2024-01-05 09:23:30 -0800

    Update file name and add comments for solutions

--- q0413/q0413_recursive_memoized.java
+++ q0413/q0413_recursive_memoized.java
@@ -13,12 +13,19 @@ class Solution {
 
     // dpHelper gives the total number of arithmetic slices ending at i
     private int dpHelper(int[] nums, int i, Map<Integer, Integer> memo) {
+
+        // base case: the length of the slice is not long enought => return 0
         if (i == 0 || i == 1)
             return 0;
+
+        // memoized case: if we have previous computed dpHelper(i)
         if (memo.containsKey(i))
             return memo.get(i);
 
+        // by default set the slice to 0
         int res = 0;
+
+        // if the number can be added:
         if (nums[i - 1] - nums[i - 2] == nums[i] - nums[i - 1]) {
             // recursively compute the # of arithmetic slices ending at i-1
             res = 1 + dpHelper(nums, i - 1, memo);
