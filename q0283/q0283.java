b8ad83dd89ba70ac90e9cce8a85246e649175b0b
Chunyue Ma <cma01@wesleyan.edu>
2024-02-15 07:28:25 -0800

    Add Java solution for 283

---
+++ q0283/q0283.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i += 1;
+            }
+        }
+        return;
+    }
+}
