3de7f7867f4999d853ccca5e3fee287f27658750
Chunyue Ma <cma01@wesleyan.edu>
2024-03-05 19:57:45 -0800

    Add comments

--- q0778/q778.java
+++ q0778/q778.java
@@ -1,12 +1,12 @@
-package q0778;
-import java.util.PriorityQueue;
-
 class Solution {
     public int swimInWater(int[][] grid) {
         int res = 0;
         int n = grid.length;
         boolean[][] visited = new boolean[n][n];
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        // Ths time to reach a cell is basically the time taken before water rise to
+        // that level so the distance to time to (x, y) = grid[x][y]
         pq.offer(new int[] { 0, 0, 0 });
         int[][] dirs = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
         while (!pq.isEmpty()) {
@@ -15,9 +15,14 @@ class Solution {
             if (!visited[x][y]) {
                 visited[x][y] = true;
                 res = Math.max(res, grid[x][y]);
-                if (x == n - 1 && y == n - 1) {
+
+                // If reached destination, directly return res since we know it is the shortest
+                // path as we are using the Dijkstra algorithm
+                if (x == n - 1 && y == n - 1)
                     return res;
-                }
+
+                // Perform BFS to explore 4 directions, if the position is within boundary
+                // And if we have not visited this position before
                 for (int[] dir : dirs) {
                     int newX = x + dir[0];
                     int newY = y + dir[1];
