173d1b73f77a48893c54055395fdb6b10fc6db6d
Chunyue Ma <cma01@wesleyan.edu>
2024-03-26 07:57:46 -0700

    Update code

--- q0041/q0041.java
+++ q0041/q0041.java
@@ -1,27 +1,31 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        boolean containsOne = false;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 1) {
-                containsOne = true;
-            } else if (nums[i] <= 0 || nums[i] > nums.length) {
-                nums[i] = 1;
-            }
+        // check if 1 is present and return 1 if it is not present
+        boolean isOnePresent = false;
+        for (int num : nums) {
+            if (num == 1)
+                isOnePresent = true;
         }
-        if (!containsOne)
+        if (!isOnePresent)
             return 1;
 
+        // use 1 to replace all 0, negative integers, and out of range integers
         for (int i = 0; i < nums.length; i++) {
-            int index = Math.abs(nums[i]) - 1;
-            if (nums[index] > 0) {
-                nums[index] *= -1;
-            }
+            if (nums[i] <= 0 || nums[i] > nums.length)
+                nums[i] = 1;
+        }
+
+        // flip the numbers to negative based on index
+        for (int num : nums) {
+            int idx = Math.abs(num) - 1;
+            // only flip once to avoid duplicated processing
+            if (nums[idx] > 0)
+                nums[idx] *= -1;
         }
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
+            if (nums[i] > 0)
                 return i + 1;
-            }
         }
         return nums.length + 1;
     }
