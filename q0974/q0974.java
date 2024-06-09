3697caa09f103974b34273e9b0d50636cb67bc91
Chunyue Ma <cma01@wesleyan.edu>
2024-06-09 11:06:00 -0700

    Update 974 for better clarity

--- q0974/q0974.java
+++ q0974/q0974.java
@@ -1,19 +1,27 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        Map<Integer, Integer> prefixSumCount = new HashMap<>();
-        int currPrefixSum = 0;
+        int n = nums.length;
         int res = 0;
+        
+        // Any number divided by k will have a mod of 0 ... k-1
+        int[] remainders = new int[k];
+        // Initialize the 0 remainder case to be 1
+        remainders[0] = 1;
+        
+        
+        int prefixSum = 0;
         for (int num : nums) {
-            currPrefixSum += num;
-            // case 1 if currPrefixSum happens to be divisible by k
-            res += ((currPrefixSum % k + k) % k == 0 ? 1 : 0);
-
-            // case 2 look for all the previous subarrays with prefixSum = currPrefixSum % k
-            int prevPrefixSum = (currPrefixSum % k + k) % k;
-            int prevPrefixCount = prefixSumCount.getOrDefault(prevPrefixSum, 0);
-            res += prevPrefixCount;
-
-            prefixSumCount.put(prevPrefixSum, prefixSumCount.getOrDefault(prevPrefixSum, 0) + 1);
+            // Update the current prefixSum
+            prefixSum += num;
+            
+            // Look for the right remainder to check (correct for negative cases)
+            int remainder = ((prefixSum % k) + k) % k;
+            
+            // Update the result of subarray counts
+            res += remainders[remainder];
+            
+            // Update the remainders count
+            remainders[remainder]++;
         }
         return res;
     }
