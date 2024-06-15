fcaad9667d272956c822b506ee3100934dc42e9b
Chunyue Ma <cma01@wesleyan.edu>
2024-06-15 10:54:08 -0700

    Add comments to the code

--- q0541/q0541.java
+++ q0541/q0541.java
@@ -1,8 +1,14 @@
 class Solution {
     public String reverseStr(String s, int k) {
         char[] charArr = s.toCharArray();
+
+        // Find the left bound of the substring to be reversed
         for (int l = 0; l < s.length(); l += 2 * k) {
+
+            // Find the right bound of the substring to be reversed
             int r = Math.min(s.length() - 1, l + k - 1);
+
+            // Reverse the substring bounded by [i, j) in place
             for (int i = l, j = r; i < j; i++, j--) {
                 char temp = charArr[i];
                 charArr[i] = charArr[j];
