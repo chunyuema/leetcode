15a0c58d9f32ae3dc26057a3a5bf02c2450da7a7
Chunyue Ma <cma01@wesleyan.edu>
2024-06-16 10:30:54 -0700

    Update Java solution for 456 with more comments

--- q0456/q0456.java
+++ q0456/q0456.java
@@ -1,18 +1,28 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
         Deque<int[]> stack = new ArrayDeque<>();
-        int currMinNum = nums[0];
+
+        // Keep track of the minPrevNum before up until an index i
+        int currMin = nums[0];
 
         for (int i = 1; i < nums.length; i++) {
-            int num = nums[i];
-            while (!stack.isEmpty() && num >= stack.peek()[0])
+            // Get the current number
+            int currNum = nums[i];
+
+            // Keep the stack monotonically decreasing to find the 3 -> 2 pattern
+            while (!stack.isEmpty() && currNum >= stack.peek()[0])
                 stack.pop();
 
-            if (!stack.isEmpty() && num > stack.peek()[1])
+            // currNum can form a 3 -> 2 pattern with the prevNum
+            // Check if the currMin of the prevNum can form 1 -> 2 with currNum
+            if (!stack.isEmpty() && currNum > stack.peek()[1])
                 return true;
 
-            stack.push(new int[] { num, currMinNum });
-            currMinNum = Math.min(currMinNum, num);
+            // Keep track of the pair of [currNum, currMin] on the stack
+            stack.push(new int[] { currNum, currMin });
+            
+            // Update the currMin for the next iterations of the numbers
+            currMin = Math.min(currNum, currMin);
         }
 
         return false;
