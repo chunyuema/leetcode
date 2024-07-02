5ae34652f0ebea19e90b177994f4080805ad47a1
Chunyue Ma <cma01@wesleyan.edu>
2024-07-02 08:15:32 -0700

    Update solution to make it simpler

--- q0309/q0309_memo.java
+++ q0309/q0309_memo.java
@@ -1,38 +1,42 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        // Edge case
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
         Map<String, Integer> memo = new HashMap<>();
-        return dpHelper(prices, 0, false, false, memo);
+        return dpHelper(prices, 0, false, memo);
     }
     
-    // Let dpHelper(i) represent the max profit you can make if you start trading from day i
-    private int dpHelper(int[] prices, int i, boolean isHoldingStock, boolean isCoolingOff, Map<String, Integer> memo) {
-        // base case: best you can do is 0 
-        if (i == prices.length) return 0;
-
-        // memoized case
-        String currState = i + "," + isHoldingStock + "," + isCoolingOff;
-        if (memo.containsKey(currState)) return memo.get(currState);
-
-        // recursive cases
-        int currPrice = prices[i];
-        
-        // case 1: buy on current day (if currently not holding stock && not in cool down mode)
-        int buy = Integer.MIN_VALUE;
-        if (!isHoldingStock && !isCoolingOff) {
-            buy = -currPrice + dpHelper(prices, i + 1, true, false, memo);
+    private int dpHelper(int[] prices, int i, boolean holdingStock, Map<String, Integer> memo) {
+        // Base case
+        if (i >= prices.length) {
+            return 0;
         }
-        
-        // case 2: sell on current day (as long as one is holding a stock)
-        int sell = Integer.MIN_VALUE;
-        if (isHoldingStock) {
-            sell = currPrice + dpHelper(prices, i + 1, false, true, memo);
-        }
-        
-        // case 3: chill on current day
-        int chill = dpHelper(prices, i + 1, isHoldingStock, false, memo);
 
-        int maxProfit = Math.max(buy, Math.max(sell, chill));
-        memo.put(currState, maxProfit);
+        // Memoization key
+        String key = i + "_" + holdingStock;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        // Recursive cases
+        // Option 1: Do nothing
+        int chill = dpHelper(prices, i + 1, holdingStock, memo);
+        
+        // Option 2: Buy stock
+        int buy = -prices[i] + dpHelper(prices, i + 1, true, memo);
+        
+        // Option 3: Sell stock
+        int sell = 0;
+        if (holdingStock) {
+            sell = prices[i] + dpHelper(prices, i + 2, false, memo); // cooling-off period after selling
+        }
+
+        // Maximize profit
+        int maxProfit = Math.max(chill, Math.max(buy, sell));
+        memo.put(key, maxProfit);
 
         return maxProfit;
     }
