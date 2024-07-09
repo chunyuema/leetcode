9cd4e2c0c7714cce7a00c96e32d5c122465b0562
Chunyue Ma <cma01@wesleyan.edu>
2024-07-08 21:57:01 -0700

    [DP - Recursive] Update code to follow recursive convention

--- q1458/q1458-top-down.java
+++ q1458/q1458-top-down.java
@@ -1,21 +1,28 @@
 class Solution {
     public int maxDotProduct(int[] nums1, int[] nums2) {
         int[][] memo = new int[nums1.length][nums2.length];
-        for (int[] row : memo)
+        for (int[] row : memo) {
             Arrays.fill(row, Integer.MIN_VALUE);
-        return dpHelper(nums1, nums2, nums1.length - 1, nums2.length - 1, memo);
+        }
+        return dpHelper(nums1, nums2, 0, 0, memo);
     }
 
-    private int dpHelper(int[] nums1, int[] nums2, int i, int j, int[][] memo) {
-        if (i < 0 || j < 0)
-            return Integer.MIN_VALUE;
-        if (memo[i][j] != Integer.MIN_VALUE)
+    private int dpHelper(final int[] nums1, final int[] nums2, int i, int j, int[][] memo) {
+        // Base case: Check if we are at the end of either nums1 or nums2
+        if (i >= nums1.length || j >= nums2.length) {
+            return Integer.MIN_VALUE; // Return a very small value as invalid
+        }
+
+        // Memoized case
+        if (memo[i][j] != Integer.MIN_VALUE) {
             return memo[i][j];
-        int sol1 = Math.max(0, dpHelper(nums1, nums2, i - 1, j - 1, memo)) + nums1[i] * nums2[j];
-        int sol2 = dpHelper(nums1, nums2, i - 1, j, memo);
-        int sol3 = dpHelper(nums1, nums2, i, j - 1, memo);
-        int res = Math.max(sol1, Math.max(sol2, sol3));
-        memo[i][j] = res;
-        return res;
+        }
+
+        // Calculate the possible solutions
+        int sol1 = nums1[i] * nums2[j] + Math.max(0, dpHelper(nums1, nums2, i + 1, j + 1, memo));
+        int sol2 = dpHelper(nums1, nums2, i + 1, j, memo);
+        int sol3 = dpHelper(nums1, nums2, i, j + 1, memo);
+        memo[i][j] = Math.max(sol1, Math.max(sol2, sol3));
+        return memo[i][j];
     }
 }
