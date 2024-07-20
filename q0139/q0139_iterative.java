ca2b4c33451245d0302ef27ef113061511c37256
Chunyue Ma <cma01@wesleyan.edu>
2024-07-20 15:19:48 -0700

    Add comments

--- q0139/q0139_iterative.java
+++ q0139/q0139_iterative.java
@@ -2,19 +2,22 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         // Let dp[i] represent if string up till index i can be segmented 
         int[] dp = new int[s.length() + 1];
-        dp[0] = 1;
-
+        dp[0] = 1; // Base case: empty string can be segmented
+        
+        // Iterate through each character of the string s
         for (int i = 1; i <= s.length(); i++) {
-            for (final String word : wordDict) {
+            // Iterate through each word in the wordDict
+            for (String word : wordDict) {
                 int j = i - word.length();
-                if (j >= 0 && 
-                    word.equals(s.substring(j, i)) &&
-                    dp[j] == 1) {
-                    dp[i] = 1;
-                    break;
+                // Check if the current substring ending at index i can be segmented
+                if (j >= 0 && word.equals(s.substring(j, i)) && dp[j] == 1) {
+                    dp[i] = 1; // Mark dp[i] as 1 if it can be segmented
+                    break; // Break out of the loop to avoid unnecessary checks
                 }
             }
         }
+        
+        // Return true if the entire string can be segmented
         return dp[s.length()] == 1;
     }
 }
