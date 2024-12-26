d900fdc6a27b9f51dd0adc49e5075f92af70a551
Chunyue Ma <cma01@wesleyan.edu>
2024-12-26 19:05:36 +0000

    Update 494 forward solution

--- q0494/q0494_top_down.java
+++ q0494/q0494_top_down.java
@@ -1,20 +1,24 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
+        // Memo to track the number of states
         Map<List<Integer>, Integer> memo = new HashMap<>();
         return dpHelper(nums, 0, 0, target, memo);
     }
 
+    // Let dp return the number of ways to build towards target if we start from a given index i
     private int dpHelper(int[] nums, int i, int currSum, int target, Map<List<Integer>, Integer> memo) {
-        // base case: if we have explored this currSum at i before, return the solution
-        List<Integer> key = Arrays.asList(i, currSum);
-        if (memo.containsKey(key)) return memo.get(key);
         
-        // recursive case: either add the current number and move on, or subtrack the current and move on
-        // recursively let dpHelper compute the value for next states: (i+1, currSum+nums[i]) (i+1, currSum-nums[i]) 
+        // Base case: if we start from the last index, simply compare the current sum with target
         if (i == nums.length) return currSum == target ? 1 : 0;
-        int res = dpHelper(nums, i+1, currSum+nums[i], target, memo) 
-                + dpHelper(nums, i+1, currSum-nums[i], target, memo);
+
+        // Memoized case: if we know the answer to reach target from this state: (i, currSum), return that
+        List<Integer> state = Arrays.asList(i, currSum);
+        if (memo.containsKey(state)) return memo.get(state);
+        
+        // Recursive case: number of ways to build to target is euqal to the sum of two decisions: + / -
+        int res = dpHelper(nums, i+1, currSum+nums[i], target, memo) + dpHelper(nums, i+1, currSum-nums[i], target, memo);
         memo.put(Arrays.asList(i, currSum), res);
+        
         return res;
     }
-}
+}}
