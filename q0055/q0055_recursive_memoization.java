01c7fa2d89a79145031d311ce25697e34ff4612f
Chunyue Ma <cma01@wesleyan.edu>
2024-12-29 20:52:59 +0000

    Update solution name + comments

--- q0055/q0055_recursive_memoization.java
+++ q0055/q0055_recursive_memoization.java
@@ -5,22 +5,25 @@ class Solution {
         return dpHelper(nums, 0, memo);
     }
 
-    // dpHelper(i) return the min number to jump to nums[n-1], if you start from index i
+    // dpHelper(i) return if you could jump from index i to the end of the array
     private boolean dpHelper(int[] nums, int i, int[] memo) {
-        // Base case: if i is already at the end
+        // Base case: if i is already at the end, return true
         if (i == nums.length - 1) return true;
 
-        // Memoized case: if dpHelper(i) has been computed before
+        // Memoized case: if dpHelper(i) has been checked before, return previous result
         if (memo[i] != -1) return memo[i] == 1;
         
         // Recursive case: for all the j's that you can reach from i, recursively check if dpHelper(j) is true
         for (int j = i + 1; j <= Math.min(nums.length-1, i + nums[i]); j++) {
+	    // If any of the index j can reach the end of the array, return true
             if (dpHelper(nums, j, memo)) {
                 memo[i] = 1;
                 return true;
             }
         }
+	// Cache the solution for furture references
         memo[i] = 0;
+	// Explored all the j's, and none of them could reach the end of the array, return false
         return false;
     }
 }
