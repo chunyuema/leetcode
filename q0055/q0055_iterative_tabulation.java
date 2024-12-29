57fd9944241b4ca2000b56850b42fc9a98e9e4b6
Chunyue Ma <cma01@wesleyan.edu>
2024-12-29 21:20:28 +0000

    Update file name + comments

--- q0055/q0055_iterative_tabulation.java
+++ q0055/q0055_iterative_tabulation.java
@@ -1,8 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int[] dp = new int[nums.length];
+        // Let dp[i] represent if index i is reachable
+	int[] dp = new int[nums.length];
         dp[0] = 1;
+
+	// For every index i, to check if index i is reachable: 
         for (int i = 0; i < nums.length; i++) {
+	    // Check all index j (where j could reach i) and see if j is reachable
             for (int j = 0; j < i; j++) {
                 if (dp[j] == 1 && j + nums[j] >= i) {
                     dp[i] = 1;
