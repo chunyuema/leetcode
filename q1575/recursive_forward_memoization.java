1bb493d7ae99483d594548fd15224f0f4e9cf678
Chunyue Ma <cma01@wesleyan.edu>
2025-01-03 21:14:10 +0000

    Update file name + add comments

--- q1575/recursive_forward_memoization.java
+++ q1575/recursive_forward_memoization.java
@@ -1,7 +1,3 @@
-package q1575;
-
-import java.util.Arrays;
-
 class Solution {
     public int countRoutes(int[] locations, int start, int finish, int fuel) {
         int[][] memo = new int[locations.length][fuel + 1];
@@ -10,12 +6,16 @@ class Solution {
         return dpHelper(locations, start, finish, fuel, memo);
     }
 
+    // Let dpHelper(currLoc, fuelLeft) represent the total number of ways from currLocation to destination 
     private int dpHelper(int[] locations, int currentLoc, int finish, int fuelLeft, int[][] memo) {
-        if (fuelLeft < 0)
-            return 0;
-        if (memo[currentLoc][fuelLeft] != -1)
-            return memo[currentLoc][fuelLeft];
+	
+	// Base Case: If there is no fuel left: 0 ways in total 
+        if (fuelLeft < 0) return 0;
 
+	// Memoized Case: If dpHelper(currLoc, fuelLeft) has been determined before, return its value
+        if (memo[currentLoc][fuelLeft] != -1) return memo[currentLoc][fuelLeft];
+        
+	// Total number of routes is equal to the sum of dpHelper(nextLoc, fuelLeft) for all nextLoc
         int totalRoutes = currentLoc == finish ? 1 : 0;
         for (int nextLoc = 0; nextLoc < locations.length; nextLoc++) {
             if (nextLoc != currentLoc) {
