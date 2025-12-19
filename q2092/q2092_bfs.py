ca3724a8a5300b5e302e219d463400edcee7def0
Chunyue Ma <cma01@wesleyan.edu>
2025-12-19 20:59:58 +0900

    update the bfs solution in python

--- q2092/q2092_bfs.py
+++ q2092/q2092_bfs.py
@@ -1,26 +1,33 @@
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        # Build graph with weighted edges
         graph = defaultdict(list)
-        for (p1, p2, time) in meetings: 
+        for p1, p2, time in meetings: 
             graph[p1].append((p2, time))
             graph[p2].append((p1, time))
+
+        # Visited set to track all the persons that the secret will reach
+        secrets_holder = set([0])
         
-        # Initialize the priority queue (based on the meeting time)
+        # Priority queue with the first person to be shared with the secret
+        # Sort by time on the pq, to ensure that earlier ones are processed first
         pq = [(0, firstPerson)]
-        for (person, time) in graph[0]: heappush(pq, (time, person))
-        
-        # Initialize the list of people who holds secrets
-        secretHolders = set([0])
+        for person, time in graph[0]: 
+            heappush(pq, (time, person))
+
+        # Performe Dijkstra algorithm to find all people reachable
         while pq:
-            # Greedily grab the earliest person who will hold secret next
-            secretHoldingTime, currPerson = heappop(pq)
+            # Extend from the meeting with the earliest time to start
+            curr_time, curr_person = heappop(pq)
 
-            # Add the person to the secret holders if they have not been added
-            if currPerson in secretHolders: continue
-            secretHolders.add(currPerson)
+            # If the curr person has been visited, skip
+            if curr_person in secrets_holder: continue
 
-            # BFS on their neighbors to identify
-            for (nextPerson, meetingTime) in graph[currPerson]: 
-                if secretHoldingTime <= meetingTime:
-                    heappush(pq, (meetingTime, nextPerson))
-        return secretHolders
+            # If the curr person has not been visisted, mark it as visited
+            # Add its neighbors to the priority queue
+            secrets_holder.add(curr_person)
+            for next_person, next_time in graph[curr_person]: 
+                if curr_time <= next_time:
+                    heappush(pq, (next_time, next_person))
+        
+        return list(visited)
